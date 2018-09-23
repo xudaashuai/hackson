897f6497aede2dcd7b46afd4e1f45e10eef9afba
yuemin.xu <yuemin.xu@airbnb.com>
2018-09-23 15:14:54 +0800

    update

--- src/compontent/MainSideBar.jsx
+++ src/compontent/MainSideBar.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 
-class MainSideBar extends React.Compontent {
-  render() {
-    const { a } = this.props.a;
-    return (
-      <div className="sidebar">
-        <div>
-          {a}
-        </div>
-        <div>
-        Item2
-        </div>
-        <div>
-        Item3
-        </div>
-        <div>
-        Item4
-        </div>
+function MainSideBar(props) {
+  const { a } = props;
+  return (
+    <div className="sidebar">
+      <div>
+        {a}
       </div>
-    );
-  }
+      <div>
+        Item2
+      </div>
+      <div>
+        Item3
+      </div>
+      <div>
+        Item4
+      </div>
+    </div>
+  );
 }
+
 
 export default MainSideBar;
